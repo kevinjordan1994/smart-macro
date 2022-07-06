791f24e3f8d2fe829f254be5a808c405b954ef8c
FingerPrintGames <kevinjordan199449@gmail.com>
2022-07-06 12:13:05 -0400

    Different structure

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 `use strict`;
 
-const express = require(`express`);
-const bodyParser = require(`body-parser`);
-const https = require(`https`);
-const { json } = require("body-parser");
-const spoonacular = require(__dirname + `/config.js`);
+function generateURL(calories = 0, carbs = 0, fat = 0, protein = 0) {
+  return `https://api.spoonacular.com/recipes/findByNutrients?apiKey=43a4dc69a4194fc9ae128504366e5e35&maxCalories=${calories}&maxCarbs=${carbs}&maxFat=${fat}&maxProtein=${protein}`;
+}
 
-const app = express();
+const testURL = generateURL(100, 40, 20, 40);
 
-app.get(`/`, function (req, res) {
-  res.send(`Test`);
-
-  // TODO: SOLVE UNEXPECTED END OF JSON INPUT
-  https.get(spoonacular.BASE_URL, (response) => {
-    response.on(`data`, (data) => {
-      const readableData = JSON.parse(data);
-      console.log(readableData.results[0].title);
-    });
+fetch(testURL)
+  .then((res) => res.json())
+  .then((data) => {
+    const meal = {
+      title: data[0].title,
+      imageURL: data[0].image,
+      calories: data[0].calories,
+      protein: data[0].protein,
+      fat: data[0].fat,
+      carbs: data[0].carbs,
+    };
+    console.log(meal);
   });
-});
-
-app.listen(3000);
