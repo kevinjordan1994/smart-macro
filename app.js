32774c10e23935dfdc3622e6c12bc2ef1ea23b6e
FingerPrintGames <kevinjordan199449@gmail.com>
2022-07-07 08:05:35 -0400

    Add user input and template for rendered recipes

--- app.js
+++ app.js
@@ -1,21 +1,87 @@
 `use strict`;
 
-function generateURL(calories = 0, carbs = 0, fat = 0, protein = 0) {
-  return `https://api.spoonacular.com/recipes/findByNutrients?apiKey=43a4dc69a4194fc9ae128504366e5e35&maxCalories=${calories}&maxCarbs=${carbs}&maxFat=${fat}&maxProtein=${protein}`;
+//Config Variables
+const BASE_URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=`;
+const API_KEY = `43a4dc69a4194fc9ae128504366e5e35`;
+let meals = [];
+
+//DOM Elements
+const ingredientsInput = document.querySelector(`.ingredients-input`);
+const caloriesInput = document.querySelector(`.calories-input`);
+const proteinInput = document.querySelector(`.protein-input`);
+const fatInput = document.querySelector(`.fat-input`);
+const carbsInput = document.querySelector(`.carbs-input`);
+const findMealsBtn = document.querySelector(`.find-meals-btn`);
+const mealsContainer = document.querySelector(`.meals-container`);
+
+function generateURL(
+  calories = 0,
+  carbs = 0,
+  fat = 0,
+  protein = 0,
+  ingredients = ``
+) {
+  return (
+    BASE_URL +
+    API_KEY +
+    `&maxCalories=${calories}&maxCarbs=${carbs}&maxFat=${fat}&maxProtein=${protein}&includeIngredients=${ingredients}&addRecipeInformation=true`
+  );
 }
 
-const testURL = generateURL(100, 40, 20, 40);
+function generateMealHTML(meal) {
+  return `<div class="meal">
+  <h1 class="title">${meal.title}</h1>
+  <img
+    src=${meal.imageURL}
+    alt=${meal.title}
+  />
+  <ul class="nutrition">
+    <li class="calories">${meal.calories} cal</li>
+    <li class="protein">${meal.protein}g protein</li>
+    <li class="fat">${meal.fat}g fat</li>
+    <li class="carbs">${meal.carbs}g carbs</li>
+  </ul>
+  <a
+    href=${meal.sourceURL}
+    target="_blank"
+    >Recipe</a
+  >
+</div>`;
+}
 
-fetch(testURL)
-  .then((res) => res.json())
-  .then((data) => {
-    const meal = {
-      title: data[0].title,
-      imageURL: data[0].image,
-      calories: data[0].calories,
-      protein: data[0].protein,
-      fat: data[0].fat,
-      carbs: data[0].carbs,
-    };
-    console.log(meal);
-  });
+function formatUserString(str) {
+  const strLower = str.toLowerCase();
+  return strLower.split(` `).join(`,`);
+}
+
+function findMeals() {
+  const resultsURL = generateURL(
+    +caloriesInput.value,
+    +carbsInput.value,
+    +fatInput.value,
+    +proteinInput.value,
+    formatUserString(ingredientsInput.value)
+  );
+  fetch(resultsURL)
+    .then((res) => res.json())
+    .then((data) => {
+      const rawMealsData = data.results;
+      rawMealsData.forEach((meal) =>
+        meals.push({
+          title: meal.title,
+          imageURL: meal.image,
+          sourceURL: meal.sourceUrl,
+          calories: Math.round(meal.nutrition.nutrients[0].amount),
+          protein: Math.round(meal.nutrition.nutrients[1].amount),
+          fat: Math.round(meal.nutrition.nutrients[2].amount),
+          carbs: Math.round(meal.nutrition.nutrients[3].amount),
+        })
+      );
+      console.log(meals);
+    });
+}
+
+findMealsBtn.addEventListener(`click`, function (e) {
+  e.preventDefault();
+  findMeals();
+});
